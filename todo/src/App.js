fb5935b4efb7272bedf39704364968f477f0bf7c
davorin <47928470+davorinpiljic@users.noreply.github.com>
2019-06-20 23:00:45 -0600

    state persist with redux-persist

--- todo/src/App.js
+++ todo/src/App.js
@@ -7,9 +7,6 @@ import InputForm from './InputForm'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
       <TodoList />
       <InputForm />
     </div>
--- todo/src/index.js
+++ todo/src/index.js
@@ -5,7 +5,20 @@ import App from './App';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import ToDoReducer from './reducer'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
 
-const store = createStore(ToDoReducer)
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+   
+  const persistedReducer = persistReducer(persistConfig, ToDoReducer)
+   
+    let store = createStore(persistedReducer)
+    let persistor = persistStore(store)
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
+
+ReactDOM.render(<Provider store={store}><PersistGate loading={null} persistor={persistor}><App /></PersistGate></Provider>, document.getElementById('root'));
